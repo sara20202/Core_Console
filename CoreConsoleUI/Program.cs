62f6aba193fd5c5f730b8952663a87fe7a1ea88d
sara20202 <zhurisara@outlook.com>
2021-05-10 12:40:49 +0200

    some init msg

---
+++ CoreConsoleUI/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CoreConsoleUI
+{
+    class Program
+    {
+        public static IConfiguration Configuration;
+        static void Main(string[] args)
+        {
+            //var builder = new ConfigurationBuilder()
+            //    .AddJsonFile("appsettings.json",optional: true, reloadOnChange: true);
+
+            //Configuration = builder.Build();
+
+            //Console.WriteLine("Hello World!");
+            ////Console.ReadLine();
+
+            //string connectionString = Configuration.GetConnectionString("Default");
+            //Console.WriteLine($"ConnectionString:{ connectionString}");
+
+
+            //string otherInfo = Configuration.GetSection("OtherInfo").Value;
+            //Console.WriteLine($" Other Info:{ otherInfo}");
+
+            //string demo1 = Configuration.GetSection("Demos").GetSection("Demo1").Value;
+            //Console.WriteLine($" Demo 1:{ demo1}");
+
+            //./Logs/LogFile.txt
+            string path = Path.Combine(".", "Logs");
+            string fullFile = Path.Combine(path, "LogFile.txt");
+
+            Directory.CreateDirectory(path);
+
+            List<string> logs = new List<string>
+            {
+                "This is my first log entry",
+                "This is my second log entry",
+                "Isn't he creative"
+            };
+            File.WriteAllLines(fullFile, logs);
+            foreach( string log in File.ReadLines(fullFile))
+            {
+                Console.WriteLine(log);
+            }
+        }
+
+    }
+}
